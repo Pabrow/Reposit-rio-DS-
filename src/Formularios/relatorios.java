30db68e113debc6cc454538953749bb61318dc12
KleyberOtenio <72840521+KleyberOtenio@users.noreply.github.com>
2020-12-05 12:30:19 -0400

    relatorios prontos

--- src/Formularios/relatorios.java
+++ src/Formularios/relatorios.java
@@ -31,8 +31,8 @@ public class relatorios extends javax.swing.JPanel {
     public relatorios() {
         initComponents();
     }
-    
-    protected void paintComponent(Graphics g){
+
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
     }
 
@@ -56,10 +56,25 @@ public class relatorios extends javax.swing.JPanel {
         jButton1.setText("jButton1");
 
         jButton3.setText("Relatório de clientes");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         jButton4.setText("Relatório de funcionários");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
 
         jButton5.setText("Relatório de vendas");
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
 
         jButton6.setText("Relatório de compras");
         jButton6.addActionListener(new java.awt.event.ActionListener() {
@@ -114,25 +129,91 @@ public class relatorios extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
-        // TODO add your handling code here:
+        String sql = "select * from Compra";
+        try {
+            Map param = new HashMap();
+            Connection con = ConexaoSQL.conectar();
+            PreparedStatement prepare = con.prepareStatement(sql);
+            ResultSet res = prepare.executeQuery();
+            JRResultSetDataSource relatResult = new JRResultSetDataSource(res);
+            JasperPrint jpPrint = JasperFillManager.fillReport("src/utilitario relatoorios/Relatorio_compras.jasper", param, relatResult);
+            JasperViewer jv = new JasperViewer(jpPrint, false);
+            jv.setVisible(true);
+            jv.toFront();
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+
     }//GEN-LAST:event_jButton6ActionPerformed
 
-    
+
     private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
-       String sql = "select * from Produto";
+        String sql = "select * from Produto";
         try {
-          Map param = new HashMap (); 
-          Connection con = ConexaoSQL.conectar();
-           PreparedStatement prepare = con.prepareStatement(sql);
+            Map param = new HashMap();
+            Connection con = ConexaoSQL.conectar();
+            PreparedStatement prepare = con.prepareStatement(sql);
             ResultSet res = prepare.executeQuery();
             JRResultSetDataSource relatResult = new JRResultSetDataSource(res);
             JasperPrint jpPrint = JasperFillManager.fillReport("src/utilitario relatoorios/Relatorio_produtos.jasper", param, relatResult);
-            JasperViewer jv = new JasperViewer(jpPrint);
+            JasperViewer jv = new JasperViewer(jpPrint, false);
             jv.setVisible(true);
-       } catch(Exception ex){
-           JOptionPane.showMessageDialog(null,ex.getMessage());
-       }
+            jv.toFront();
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
     }//GEN-LAST:event_jButton7ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        String sql = "select * from Funcionario";
+        try {
+            Map param = new HashMap();
+            Connection con = ConexaoSQL.conectar();
+            PreparedStatement prepare = con.prepareStatement(sql);
+            ResultSet res = prepare.executeQuery();
+            JRResultSetDataSource relatResult = new JRResultSetDataSource(res);
+            JasperPrint jpPrint = JasperFillManager.fillReport("src/utilitario relatoorios/Relatorio_funcionario.jrxml", param, relatResult);
+            JasperViewer jv = new JasperViewer(jpPrint, false);
+            jv.setVisible(true);
+            jv.toFront();
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        String sql = "select * from Venda";
+        try {
+            Map param = new HashMap();
+            Connection con = ConexaoSQL.conectar();
+            PreparedStatement prepare = con.prepareStatement(sql);
+            ResultSet res = prepare.executeQuery();
+            JRResultSetDataSource relatResult = new JRResultSetDataSource(res);
+            JasperPrint jpPrint = JasperFillManager.fillReport("src/utilitario relatoorios/Relatorio_vendas.jrxml", param, relatResult);
+            JasperViewer jv = new JasperViewer(jpPrint, false);
+            jv.setVisible(true);
+            jv.toFront();
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+    }//GEN-LAST:event_jButton5ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        String sql = "select * From Cliente";
+        try {
+            Map param = new HashMap();
+            Connection con = ConexaoSQL.conectar();
+            PreparedStatement prepare = con.prepareStatement(sql);
+            ResultSet res = prepare.executeQuery();
+            JRResultSetDataSource relatResult = new JRResultSetDataSource(res);
+            JasperPrint jpPrint = JasperFillManager.fillReport("src/utilitario relatoorios/Relatorio_cliente.jrxml", param, relatResult);
+            JasperViewer jv = new JasperViewer(jpPrint, false);
+            jv.setVisible(true);
+            jv.toFront();
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+    }//GEN-LAST:event_jButton3ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
